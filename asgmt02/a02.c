44945ccc872771afd9fe4510a6884750b8e4f4c1
Chow <kahoo.chow@syccuxas01.pcc.edu>
2020-08-18 10:32:05 -0700

    final submission

--- asgmt02/a02.c
+++ asgmt02/a02.c
@@ -1,12 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+
+double pythagorean(double x, double y)
+{
+	double	distance;
+
+	asm(							// contents of fp stack:
+		"	fldl	%[x]		\n"	// x ...
+		"	fld		%%st		\n"
+		"	fmulp				\n"
+
+		"	fldl	%[y]		\n"	// y x ...
+		"	fld		%%st		\n"
+		"	fmulp				\n"
+
+		"	faddp				\n"	// distance ...
+		"	fsqrt				\n"
+		"	fstpl	%[distance]	 	\n"	// ...
+
+		:	[distance]	"=m"	(distance)	// outputs
+		:	[x]		"m"		(x),	// inputs
+			[y]		"m"		(y)
+	);
+
+	return distance;
+}
 
 int main(int argc, char **argv)
 {
-	int i, in, out = 0;
+	int i = 0; 
+	int in = 0;
 	int darts  = atof(argv[1]);
-	double x, y, pi;
+	double x, y, pi, distance;
 	printf("CS 201 - Assignment 2 - Ka Hoo Chow\n");
 
 	srand48(0);
@@ -15,16 +40,17 @@ int main(int argc, char **argv)
 	{
 		x = drand48();
 		y = drand48();
-		if(sqrt(pow(x,2) + pow(y,2)) <= 1)
+		distance = pythagorean(x, y);
+		if(distance <= 1)
 		{
 			in++;
 		}
 
-		pi = 4.0 * in / darts;
-		
 		i++;
 	}
-	printf("%i darts : pi = %f \n", darts, pi);
+		
+	pi = 4.0 * in / darts;
+	printf("%i darts : pi = %.10f \n", darts, pi);
 
 	
 	return 0;
